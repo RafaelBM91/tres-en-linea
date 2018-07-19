cb9cae16b6e199e2a56c4dd789880cf027b38de8
Rafael Briceño <rafael.briceo@metricaandina.com>
2018-07-19 15:43:17 -0500

    create

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+
+var BUILD_DIR = path.resolve(__dirname + 'public');
+var APP_DIR = path.resolve(__dirname + 'src');
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'development',
+    target: 'node',
+    output: {
+      path: BUILD_DIR,
+      filename: 'js/bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.tsx']
+    },
+    target: 'web',
+    module: {
+      rules: [
+          {
+            test: /\.tsx?$/,
+            loaders: 'awesome-typescript-loader'
+          },
+      ]
+    },
+    devtool: 'source-map'
+  };
